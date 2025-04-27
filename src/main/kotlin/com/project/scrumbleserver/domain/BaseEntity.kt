708cd36abd1edfa7a6959fc59b30f3c900fa259e
Jwhyee <kawnsdud@naver.com>
2025-04-27 16:11:29 +0900

    fix : BaseEntity regDate insert error

--- src/main/kotlin/com/project/scrumbleserver/domain/BaseEntity.kt
+++ src/main/kotlin/com/project/scrumbleserver/domain/BaseEntity.kt
@@ -1,12 +1,19 @@
 package com.project.scrumbleserver.domain
 
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
 
     @CreatedDate
-    var regDate: LocalDateTime = LocalDateTime.MIN
+    @Column(name = "reg_date", updatable = false)
+    var regDate: LocalDateTime = LocalDateTime.now()
 
     private var isDeleted: Boolean = false
 
--- src/main/kotlin/com/project/scrumbleserver/domain/productBacklog/ProductBacklog.kt
+++ src/main/kotlin/com/project/scrumbleserver/domain/productBacklog/ProductBacklog.kt
@@ -12,7 +12,6 @@ import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
-@Entity
 @Table(name = "product_backlog")
 class ProductBacklog(
     @Id
