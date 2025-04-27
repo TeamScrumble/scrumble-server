b988b973eb339ef4b4e7e01bd5fabf70b7ea248d
Jwhyee <kawnsdud@naver.com>
2025-04-27 15:50:20 +0900

    feat #5 : get all list by project rowid

--- src/main/kotlin/com/project/scrumbleserver/controller/productBacklog/ProductBacklogController.kt
+++ src/main/kotlin/com/project/scrumbleserver/controller/productBacklog/ProductBacklogController.kt
@@ -1,8 +1,10 @@
 package com.project.scrumbleserver.controller.productBacklog
 
+import com.project.scrumbleserver.api.productBacklog.ApiGetAllProductBacklog
 import com.project.scrumbleserver.api.productBacklog.ApiPostProductBacklog
 import com.project.scrumbleserver.global.api.ApiResponse
 import com.project.scrumbleserver.service.productBacklog.ProductBacklogService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -20,5 +22,14 @@ class ProductBacklogController(
         val response = ApiPostProductBacklog.Response(productBacklogRowid)
 
         return ApiResponse.of(response)
+    }
+
+    @GetMapping(ApiGetAllProductBacklog.PATH)
+    fun findAll(
+        @RequestBody request: ApiGetAllProductBacklog.Request
+    ): ApiResponse<List<ApiGetAllProductBacklog.Response>> {
+        val productBacklogs = productBacklogService.findAll(request.projectRowid)
+
+        return ApiResponse.of(productBacklogs)
     }
 }
--- src/main/kotlin/com/project/scrumbleserver/repository/productBacklog/ProductBacklogRepository.kt
+++ src/main/kotlin/com/project/scrumbleserver/repository/productBacklog/ProductBacklogRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface ProductBacklogRepository : JpaRepository<ProductBacklog, Long> {
+    fun findAllByProjectRowid(productRowid: Long): List<ProductBacklog>
 }
