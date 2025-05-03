73ac955f090b344b69de3590954ce99e33367d27
wwan13 <wwan13@naver.com>
2025-05-04 00:10:24 +0900

    refactor : change service method have Api DTO

--- src/main/kotlin/com/project/scrumbleserver/service/project/ProjectService.kt
+++ src/main/kotlin/com/project/scrumbleserver/service/project/ProjectService.kt
@@ -1,20 +1,35 @@
 package com.project.scrumbleserver.service.project
 
+import com.project.scrumbleserver.api.project.ApiGetAllProject
+import com.project.scrumbleserver.api.project.ApiPostProject
 import com.project.scrumbleserver.domain.project.Project
 import com.project.scrumbleserver.repository.project.ProjectRepository
-import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
-@Repository
+@Service
 class ProjectService(
-    private val projectRepository: ProjectRepository
+    private val projectRepository: ProjectRepository,
 ) {
-    fun insert(title: String): Long {
-        val project = Project(title = title)
+    @Transactional
+    fun insert(
+        request: ApiPostProject.Request
+    ): ApiPostProject.Response {
+        val project = Project(title = request.title)
         projectRepository.save(project)
-        return project.rowid
+        return ApiPostProject.Response(project.rowid)
     }
 
-    fun findAll(): List<Project> {
-        return projectRepository.findAll()
+    @Transactional(readOnly = true)
+    fun findAll(): ApiGetAllProject.Response {
+        return ApiGetAllProject.Response(
+            projectRepository.findAll().map {
+                ApiGetAllProject.Response.Project(
+                    rowid = it.rowid,
+                    title = it.title,
+                    regDate = it.regDate
+                )
+            }
+        )
     }
 }
