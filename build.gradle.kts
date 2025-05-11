f1efa1bc9612767f1d4bba19eea85f65ee19f4f2
Jwhyee <kawnsdud@naver.com>
2025-05-11 21:40:27 +0900

    Springdocs 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
 
     runtimeOnly("com.mysql:mysql-connector-j")
