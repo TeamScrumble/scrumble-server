3d7f40512d3c00701f2ffa8ec830b8539572a1bf
Jwhyee <kawnsdud@naver.com>
2025-05-25 22:26:40 +0900

    feat : save product backlog tags when product backlog saved

---
+++ src/main/kotlin/com/project/scrumbleserver/domain/tag/ProductBacklogTag.kt
@@ -0,0 +1,32 @@
+package com.project.scrumbleserver.domain.tag
+
+import com.project.scrumbleserver.domain.BaseEntity
+import com.project.scrumbleserver.domain.productBacklog.ProductBacklog
+import com.project.scrumbleserver.domain.productBacklog.ProductBacklogPriority
+import com.project.scrumbleserver.domain.project.Project
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity(name = "product_backlog_tag")
+class ProductBacklogTag(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_backlog_tag_rowid")
+    var rowid: Long = 0L,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_backlog_rowid", nullable = false)
+    val productBacklog: ProductBacklog,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag", nullable = false)
+    val tag: Tag
+) : BaseEntity()
