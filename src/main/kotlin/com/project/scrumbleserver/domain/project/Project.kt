bf75c93a40bd390770805aaa2396c2f12f80b15e
Jwhyee <kawnsdud@naver.com>
2025-05-25 22:12:43 +0900

    refactor : add relation project to tag

--- src/main/kotlin/com/project/scrumbleserver/domain/project/Project.kt
+++ src/main/kotlin/com/project/scrumbleserver/domain/project/Project.kt
@@ -1,11 +1,15 @@
 package com.project.scrumbleserver.domain.project
 
 import com.project.scrumbleserver.domain.BaseEntity
+import com.project.scrumbleserver.domain.productBacklog.ProductBacklog
+import com.project.scrumbleserver.domain.tag.Tag
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 
 @Entity
 class Project(
@@ -19,4 +23,7 @@ class Project(
 
     @Column(nullable = true, length = 150)
     var description: String,
+
+    @OneToMany(mappedBy = "project", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val tags: MutableList<Tag> = mutableListOf()
 ) : BaseEntity()
