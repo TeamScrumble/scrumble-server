cc7076b81014771ab289f3abf1286ea9fc752b93
Jwhyee <kawnsdud@naver.com>
2025-05-25 22:30:33 +0900

    add : relation ProductBacklog to ProductBacklogTag

--- src/main/kotlin/com/project/scrumbleserver/domain/productBacklog/ProductBacklog.kt
+++ src/main/kotlin/com/project/scrumbleserver/domain/productBacklog/ProductBacklog.kt
@@ -2,6 +2,8 @@ package com.project.scrumbleserver.domain.productBacklog
 
 import com.project.scrumbleserver.domain.BaseEntity
 import com.project.scrumbleserver.domain.project.Project
+import com.project.scrumbleserver.domain.tag.ProductBacklogTag
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 
 @Entity(name = "product_backlog")
 class ProductBacklog(
@@ -32,5 +35,8 @@ class ProductBacklog(
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    var priority: ProductBacklogPriority = ProductBacklogPriority.NONE
+    var priority: ProductBacklogPriority = ProductBacklogPriority.NONE,
+
+    @OneToMany(mappedBy = "productBacklog", cascade = [CascadeType.ALL])
+    val tags: MutableList<ProductBacklogTag> = mutableListOf()
 ) : BaseEntity()
