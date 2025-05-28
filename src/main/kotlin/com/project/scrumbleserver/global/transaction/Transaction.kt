7da679d6d0effd9a271731d8126deeefe8c3aed9
wwan13 <wwan13@naver.com>
2025-05-28 20:05:14 +0900

    feat : add lambda style transaction component

---
+++ src/main/kotlin/com/project/scrumbleserver/global/transaction/Transaction.kt
@@ -0,0 +1,23 @@
+package com.project.scrumbleserver.global.transaction
+
+import com.project.scrumbleserver.global.excception.ServerException
+import org.springframework.stereotype.Component
+import org.springframework.transaction.support.TransactionTemplate
+
+@Component
+class Transaction(
+    private val transactionTemplate: TransactionTemplate,
+) {
+    operator fun <T> invoke(action: () -> T): T {
+        return transactionTemplate.execute {
+            action()
+        } ?: throw ServerException("transaction execute failed")
+    }
+
+    fun <T> readOnly(readOnlyAction: () -> T): T {
+        transactionTemplate.isReadOnly = true
+        return transactionTemplate.execute {
+            readOnlyAction()
+        } ?: throw ServerException("transaction execute failed")
+    }
+}
