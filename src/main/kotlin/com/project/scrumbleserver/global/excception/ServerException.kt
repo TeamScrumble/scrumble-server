dc6bd9ee8cd094f9dc4b8e37b0e197e070f9f3ba
Jwhyee <kawnsdud@naver.com>
2025-06-01 11:24:26 +0900

    fix : Exception type change

--- src/main/kotlin/com/project/scrumbleserver/global/excception/ServerException.kt
+++ src/main/kotlin/com/project/scrumbleserver/global/excception/ServerException.kt
@@ -2,4 +2,4 @@ package com.project.scrumbleserver.global.excception
 
 class ServerException(
     override val message: String = "unknown error occurred"
-) : Exception(message)
+) : RuntimeException(message)
