a3eb87d661dbb0a1cfd0579e1925f958a3efec0e
wwan13 <wwan13@naver.com>
2025-06-20 14:55:31 +0900

    feat : implementing code related to authentication/authorization

---
+++ src/main/kotlin/com/project/scrumbleserver/security/RequestUserRowid.kt
@@ -0,0 +1,5 @@
+package com.project.scrumbleserver.security
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RequestUserRowid
---
+++ src/main/kotlin/com/project/scrumbleserver/security/UserIdResolver.kt
@@ -0,0 +1,32 @@
+package com.project.scrumbleserver.security
+
+import com.project.scrumbleserver.global.exception.UnauthorizedException
+import org.springframework.core.MethodParameter
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+class UserIdResolver : HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(RequestUserRowid::class.java)
+            && parameter.parameterType == Long::class.java
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: org.springframework.web.bind.support.WebDataBinderFactory?,
+    ): Any? {
+        val auth = SecurityContextHolder.getContext().authentication
+
+        if (auth is UsernamePasswordAuthenticationToken && auth.principal is Long) {
+            return auth.principal as Long
+        }
+
+        throw UnauthorizedException("Unauthorized")
+    }
+}
