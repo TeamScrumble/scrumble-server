7a5dd3c1d6370d35dc69f973535fa004aca5ff43
Jwhyee <kawnsdud@naver.com>
2025-06-28 17:32:05 +0900

    feat : add member lookup by email in ProjectInviteService

---
+++ src/main/kotlin/com/project/scrumbleserver/domain/member/MemberRepository.kt
@@ -0,0 +1,9 @@
+package com.project.scrumbleserver.domain.member
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByEmail(email: String): Member?
+}
--- src/main/kotlin/com/project/scrumbleserver/service/projectInvite/ProjectInviteService.kt
+++ src/main/kotlin/com/project/scrumbleserver/service/projectInvite/ProjectInviteService.kt
@@ -1,8 +1,8 @@
 package com.project.scrumbleserver.service.projectInvite
 
-import com.project.scrumbleserver.domain.member.Member
+import com.project.scrumbleserver.domain.member.MemberRepository
 import com.project.scrumbleserver.domain.projectInvite.ProjectInvite
-import com.project.scrumbleserver.global.excception.BusinessException
+import com.project.scrumbleserver.global.exception.BusinessException
 import com.project.scrumbleserver.global.transaction.Transaction
 import com.project.scrumbleserver.repository.project.ProjectRepository
 import com.project.scrumbleserver.repository.projectInvite.ProjectInviteRepository
@@ -13,7 +13,8 @@ import org.springframework.stereotype.Service
 class ProjectInviteService(
     private val transaction: Transaction,
     private val projectRepository: ProjectRepository,
-    private val projectInviteRepository: ProjectInviteRepository
+    private val projectInviteRepository: ProjectInviteRepository,
+    private val memberRepository: MemberRepository
 ) {
     fun invite(
         projectRowid: Long,
@@ -23,14 +24,12 @@ class ProjectInviteService(
             val project = projectRepository.findByIdOrNull(projectRowid)
                 ?: throw BusinessException("프로젝트를 찾을 수 없습니다.")
 
+            val member = memberRepository.findByEmail(email) ?: throw BusinessException("회원을 찾을 수 없습니다.")
+
             val projectInvite = projectInviteRepository.save(
                 ProjectInvite(
                     project = project,
-                    member = Member(
-                        email = email,
-                        nickname = "",
-                        job = ""
-                    )
+                    member = member
                 )
             )
 
