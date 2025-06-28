fa0702630158215ed693b6eb387a8d9a70aeaa5d
Jwhyee <kawnsdud@naver.com>
2025-06-28 16:41:15 +0900

    feat : add ProjectMember entity with permissions

---
+++ src/main/kotlin/com/project/scrumbleserver/domain/projectMember/ProjectMember.kt
@@ -0,0 +1,38 @@
+package com.project.scrumbleserver.domain.projectMember
+
+import com.project.scrumbleserver.domain.BaseEntity
+import com.project.scrumbleserver.domain.member.Member
+import com.project.scrumbleserver.domain.productBacklog.ProductBacklog
+import com.project.scrumbleserver.domain.productBacklog.ProductBacklogPriority
+import com.project.scrumbleserver.domain.project.Project
+import com.project.scrumbleserver.domain.tag.Tag
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity(name = "project_member")
+class ProjectMember(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_member_rowid")
+    var rowid: Long = 0L,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_rowid", nullable = false)
+    val project: Project,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_rowid", nullable = false)
+    val member: Member,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var permission: ProductMemberPermission = ProductMemberPermission.CAN_VIEW
+) : BaseEntity()
