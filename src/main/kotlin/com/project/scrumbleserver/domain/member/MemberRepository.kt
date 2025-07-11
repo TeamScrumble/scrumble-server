129cb20111407952d894396039ca645d90380762
Jwhyee <kawnsdud@naver.com>
2025-07-11 22:59:21 +0900

    사용하지 않는 Repository 파일 제거

--- src/main/kotlin/com/project/scrumbleserver/domain/member/MemberRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.project.scrumbleserver.domain.member
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MemberRepository : JpaRepository<Member, Long> {
-    fun findByEmail(email: String): Member?
-}
