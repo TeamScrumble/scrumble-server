ec37ef415479307932a51f09687311843b8b325e
wwan13 <wwan13@naver.com>
2025-07-21 21:12:56 +0900

    feat : add API query all the members included in the project

--- src/main/kotlin/com/project/scrumbleserver/repository/projectMember/ProjectMemberRepository.kt
+++ src/main/kotlin/com/project/scrumbleserver/repository/projectMember/ProjectMemberRepository.kt
@@ -4,10 +4,14 @@ import com.project.scrumbleserver.domain.member.Member
 import com.project.scrumbleserver.domain.project.Project
 import com.project.scrumbleserver.domain.projectMember.ProjectMember
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ProjectMemberRepository : JpaRepository<ProjectMember, Long> {
     fun findByProjectAndMember(project: Project, member: Member): ProjectMember?
-    fun findByProject(project: Project): List<ProjectMember>
+
+    @Query("SELECT pm FROM project_member pm JOIN FETCH pm.member WHERE pm.project = :project")
+    fun findByProject(@Param("project") project: Project): List<ProjectMember>
 }
