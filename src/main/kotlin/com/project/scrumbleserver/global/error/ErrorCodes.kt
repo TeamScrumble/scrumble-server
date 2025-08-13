5f6148bad1105740c525177069f97e76c65726f3
Jwhyee <kawnsdud@naver.com>
2025-08-13 22:54:58 +0900

    refactor : BusinessException's message to ErrorCode type

--- src/main/kotlin/com/project/scrumbleserver/global/error/ErrorCodes.kt
+++ src/main/kotlin/com/project/scrumbleserver/global/error/ErrorCodes.kt
@@ -1,13 +1,23 @@
 package com.project.scrumbleserver.global.error
 
-enum class ProjectErrorCode(
-    val code: String,
+interface ErrorCode {
+    val code: String
     val description: String
-) {
-    // 공통
-    MEMBER_NOT_FOUND_MSG("COMMON_001", "존재하지 않는 회원입니다."),
-    PROJECT_NOT_FOUND_MSG("COMMON_002", "존재하지 않는 프로젝트입니다."),
-    NOT_PROJECT_MEMBER_MSG("COMMON_003", "프로젝트에 속한 회원이 아닙니다."),
-    MUST_HAVE_AT_LEAST_ONE_OWNER_MSG("COMMON_004", "프로젝트에 최소 한 명 이상의 OWNER 권한이 존재해야 합니다."),
-    INSUFFICIENT_PROJECT_PERMISSION_MSG("COMMON_005", "해당 작업을 수행할 권한이 없습니다."),
+}
+
+enum class CommonError(
+    override val code: String,
+    override val description: String
+): ErrorCode {
+    MEMBER_NOT_FOUND("C001", "존재하지 않는 회원입니다.")
+}
+
+enum class ProjectError(
+    override val code: String,
+    override val description: String
+): ErrorCode {
+    PROJECT_NOT_FOUND("P001", "존재하지 않는 프로젝트입니다."),
+    NOT_PROJECT_MEMBER("P002", "프로젝트에 속한 회원이 아닙니다."),
+    MUST_HAVE_AT_LEAST_ONE_OWNER("P003", "프로젝트에 최소 한 명 이상의 OWNER 권한이 존재해야 합니다."),
+    INSUFFICIENT_PROJECT_PERMISSION("P004", "해당 작업을 수행할 권한이 없습니다."),
 }
